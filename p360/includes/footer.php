a3a01d0f0614de341941083d306066dd1997f05a
AlexFdez85 <alejandro@gpoferro.com>
2025-05-07 20:28:01 -0600

    Delete p360 directory

--- p360/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-// p360/includes/footer.php
-?>
-<footer style="background: var(--color-light); color: var(--color-muted); text-align:center; padding:1rem; font-size:0.9rem;">
-  &copy; <?= date('Y') ?> Pinturas Grupo FERRO. Todos los derechos reservados.
-</footer>
